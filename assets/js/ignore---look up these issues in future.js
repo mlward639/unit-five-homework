eb402beefbf97a03d450eeb8352c0db7286ed6a9
Maggie Ward <mlward639@gmail.com>
2021-06-25 20:32:37 -0400

    correct print functionality

---
+++ assets/js/ignore---look up these issues in future.js
@@ -0,0 +1,68 @@
+/* why does this work: 
+i=1
+blankInputArray[i] = document.createElement("INPUT");
+blankInputArray[i].setAttribute("type", "text");
+blankActivityArray[i].appendChild(blankInputArray[i]);
+blankInputArray[i].classList.add("blank-input")
+
+and this work: 
+blankInputArray[1] = document.createElement("INPUT");
+blankInputArray[1].setAttribute("type", "text");
+blankActivityArray[1].appendChild(blankInputArray[1]);
+blankInputArray[1].classList.add("blank-input")
+
+but not this: 
+function addBlankInput () {
+    for (var i = 0; i < blankActivityArray.length; i++) {
+        blankInputArray[i] = document.createElement("INPUT");}
+    for (var i = 0; i < blankActivityArray.length; i++) {
+        blankInputArray[i].setAttribute("type", "text");}
+    for (var i = 0; i < blankActivityArray.length; i++) {
+            blankInputArray[i].classList.add("blank-input")
+            blankActivityArray[i].appendChild(blankInputArray[i]);}
+
+}
+//be able to add text to calendar
+function addBlankInput () {
+    for (var i = 0; i <= blankActivityArray.length; i++) {
+        blankInputArray[i] = document.createElement("INPUT");}
+    for (var i = 0; i <= blankActivityArray.length; i++) {
+        blankInputArray[i].setAttribute("type", "text");}
+    for (var i = 0; i <= blankActivityArray.length; i++) {
+            blankInputArray[i].classList.add("blank-input")
+    }
+    for (var i = 0; i <= blankActivityArray.length; i++) {
+        blankActivityArray[i].appendChild(blankInputArray[i]);}  //why does this give error msg on console log
+
+}
+addBlankInput(); 
+
+function addBlankInput () {
+    for (var i = 0; i < blankActivityArray.length; i++) {
+        blankInputArray[i] = document.createElement("INPUT");}
+    for (var i = 0;  i < blankActivityArray.length; i++) {
+        blankInputArray[i].setAttribute("type", "text");}
+    for (var i = 0;  i < blankActivityArray.length; i++) {
+            blankInputArray[i].classList.add("blank-input")
+            blankActivityArray[i].appendChild(blankInputArray[i]);}
+
+} */
+
+//click button and save to local storage, cuts out all the button add listeners, not working quite right yet
+$(document).on("click", "button", function (event) {
+    console.log("this.value: " + this.value);
+    localStorage.setItem(this.value, JSON.stringify(this.value));
+  });
+
+//get and print saved data
+document.querySelector("#hour0").value = JSON.parse(localStorage.getItem("this.value")) || "";
+document.querySelector("#hour1").value = JSON.parse(localStorage.getItem("this.value")) || "";
+document.querySelector("#hour2").value = JSON.parse(localStorage.getItem("this.value")) || "";
+document.querySelector("#hour3").value = JSON.parse(localStorage.getItem("this.value")) || "";
+document.querySelector("#hour4").value = JSON.parse(localStorage.getItem("this.value")) || "";
+document.querySelector("#hour5").value = JSON.parse(localStorage.getItem("this.value")) || "";
+document.querySelector("#hour6").value = JSON.parse(localStorage.getItem("this.value")) || "";
+document.querySelector("#hour7").value = JSON.parse(localStorage.getItem("this.value")) || "";
+document.querySelector("#hour8").value = JSON.parse(localStorage.getItem("this.value")) || "";
+
+ 
--- assets/js/script.js
+++ assets/js/script.js
@@ -8,8 +8,8 @@ var blankActivity6 = document.querySelector(".blank-activity-6");
 var blankActivity7 = document.querySelector(".blank-activity-7");
 var blankActivity8 = document.querySelector(".blank-activity-8");
 var blankActivity9 = document.querySelector(".blank-activity-9");
-var currentDayDisplay = document.querySelector("#currentDay"); 
-var tester = document.querySelector(".hour")
+var currentDayDisplay = document.querySelector("#currentDay");
+var tester = document.querySelector(".hour");
 
 var nineAM = document.querySelector("#nineAM");
 var tenAM = document.querySelector("#tenAM");
@@ -21,15 +21,15 @@ var threePM = document.querySelector("#threePM");
 var fourPM = document.querySelector("#fourPM");
 var fivePM = document.querySelector("#fivePM");
 
-var btn1 = document.querySelector('.btn1');
-var btn2 = document.querySelector('.btn2');
-var btn3 = document.querySelector('.btn3');
-var btn4 = document.querySelector('.btn4');
-var btn5 = document.querySelector('.btn5');
-var btn6 = document.querySelector('.btn6');
-var btn7 = document.querySelector('.btn7');
-var btn8 = document.querySelector('.btn8');
-var btn9 = document.querySelector('.btn9');
+var btn1 = document.querySelector(".btn1");
+var btn2 = document.querySelector(".btn2");
+var btn3 = document.querySelector(".btn3");
+var btn4 = document.querySelector(".btn4");
+var btn5 = document.querySelector(".btn5");
+var btn6 = document.querySelector(".btn6");
+var btn7 = document.querySelector(".btn7");
+var btn8 = document.querySelector(".btn8");
+var btn9 = document.querySelector(".btn9");
 
 nineAM = 09;
 tenAM = 10;
@@ -41,66 +41,75 @@ threePM = 15;
 fourPM = 16;
 fivePM = 17;
 
-var blankInput1=' ';
-var blankInput2=' ';
-var blankInput3=' ';
-var blankInput4=' ';
-var blankInput5=' ';
-var blankInput6=' ';
-var blankInput7=' ';
-var blankInput8=' ';
-var blankInput9=' ';
-
 var rightNow;
 var todayMilitary;
 var numberTodayMilitary;
 
-var blankInputArray = [blankInput1, blankInput2, blankInput3, blankInput4, blankInput5, blankInput6, blankInput7, blankInput8, blankInput9];
-var blankActivityArray = [blankActivity1, blankActivity2, blankActivity3, blankActivity4, blankActivity5, blankActivity6, blankActivity7, blankActivity8, blankActivity9];
-var colorNumbersArray = [nineAM, tenAM, elevenAM, twelvePM, onePM, twoPM, threePM, fourPM, fivePM];
+var blankInputArray = [];
+var blankActivityArray = [
+  blankActivity1,
+  blankActivity2,
+  blankActivity3,
+  blankActivity4,
+  blankActivity5,
+  blankActivity6,
+  blankActivity7,
+  blankActivity8,
+  blankActivity9,
+];
+var colorNumbersArray = [
+  nineAM,
+  tenAM,
+  elevenAM,
+  twelvePM,
+  onePM,
+  twoPM,
+  threePM,
+  fourPM,
+  fivePM,
+];
+var hour0;
 
 //add date to header and calculate military time of hour so can compare later for background colors
 function displayCurrentTime() {
-    var today = moment()
-    $(currentDayDisplay).text(today.format("dddd, MMM Do, YYYY")); 
-    rightNow = today.format('hh:mm:ss a');
-    todayMilitary = today.format("kk");
-    numberTodayMilitary = parseInt(todayMilitary);
-  }
-displayCurrentTime()
+  var today = moment();
+  $(currentDayDisplay).text(today.format("dddd, MMM Do, YYYY"));
+  rightNow = today.format("hh:mm:ss a");
+  todayMilitary = today.format("kk");
+  numberTodayMilitary = parseInt(todayMilitary);
+}
+displayCurrentTime();
 setInterval(displayCurrentTime, 1000);
 
 //add input so can add text
-function addBlankInput () {
-    for (var i = 0; i < blankActivityArray.length; i++) {
-        blankInputArray[i] = document.createElement("INPUT");
-        blankInputArray[i].setAttribute("type", "text");
-        blankInputArray[i].classList.add("blank-input")
-        blankInputArray[i].setAttribute("id", "hour"+i );
-        blankActivityArray[i].appendChild(blankInputArray[i]);
-        console.log(blankActivityArray[i])
-    }
-    return; //need return??
+function addBlankInput() {
+  for (var i = 0; i < blankActivityArray.length; i++) {
+    blankInputArray[i] = document.createElement("INPUT");
+    blankInputArray[i].setAttribute("type", "text");
+    blankInputArray[i].classList.add("blank-input");
+    blankInputArray[i].setAttribute("id", "hour" + i);
+    blankActivityArray[i].appendChild(blankInputArray[i]);
+    console.log(blankActivityArray[i]);
+  }
 }
-addBlankInput()
+addBlankInput();
 
-console.log(blankInputArray[0], blankInputArray[1])
+console.log(blankInputArray[0], blankInputArray[1]);
 
 //compare current hour vs hour of each timeblock to determine color
-function colorChange (){
-    for (var i = 0; i < colorNumbersArray.length; i++){
-        if (numberTodayMilitary === colorNumbersArray[i]) {
-            blankActivityArray[i].style.backgroundColor = "#ff6961";
-            blankInputArray[i].style.color = "white";
-        } else if (numberTodayMilitary < colorNumbersArray[i]) {
-            blankActivityArray[i].style.backgroundColor = "#77dd77";
-            blankInputArray[i].style.color = "white";
-        } else if (numberTodayMilitary > colorNumbersArray[i]) {
-            blankActivityArray[i].style.backgroundColor = "#d3d3d3";
-        }
-    } 
-    return; //need return??
-} 
+function colorChange() {
+  for (var i = 0; i < colorNumbersArray.length; i++) {
+    if (numberTodayMilitary === colorNumbersArray[i]) {
+      blankActivityArray[i].style.backgroundColor = "#ff6961";
+      blankInputArray[i].style.color = "white";
+    } else if (numberTodayMilitary < colorNumbersArray[i]) {
+      blankActivityArray[i].style.backgroundColor = "#77dd77";
+      blankInputArray[i].style.color = "white";
+    } else if (numberTodayMilitary > colorNumbersArray[i]) {
+      blankActivityArray[i].style.backgroundColor = "#d3d3d3";
+    }
+  }
+}
 colorChange();
 
 //when click save button, save to local storage
@@ -110,61 +119,58 @@ colorChange();
     console.log("first input box: " + blankInput1.value)
     return; //need return??
 } */
-$(document).on("click","button", function(event){
-console.log(this.value)
-localStorage.setItem(this.value, JSON.stringify(this.value));
-})
+
 //when open page, pull from local storage for last input
-/*btn1.addEventListener("click", function getSaved(){
+btn1.addEventListener("click", function getSaved(){
     localStorage.setItem("savedInput9AM", JSON.stringify(blankInputArray[0].value));
     console.log("btn 1 worked :  " + blankInputArray[0].value)
-}) */
-btn2.addEventListener("click", function getSaved(){
-    localStorage.setItem("savedInput10AM", blankInputArray[1].value);
-    console.log("btn 2 worked :  " + blankInputArray[1].value)
 })
-btn3.addEventListener("click", function getSaved(){
-    localStorage.setItem("savedInput11AM", blankInputArray[2].value);
-    console.log("btn 3 worked :  " + blankInputArray[2].value)
-})
-btn4.addEventListener("click", function getSaved(){
-    localStorage.setItem("savedInput12PM", blankInputArray[3].value);
-    console.log("btn 4 worked :  " + blankInputArray[3].value)
-})
-btn5.addEventListener("click", function getSaved(){
-    localStorage.setItem("savedInput1PM", blankInputArray[4].value);
-    console.log("btn 5 worked :  " + blankInputArray[4].value)
-})
-btn6.addEventListener("click", function getSaved(){
-    localStorage.setItem("savedInput2PM", blankInputArray[5].value);
-    console.log("btn 6 worked :  " + blankInputArray[5].value)
-})
-btn7.addEventListener("click", function getSaved(){
-    localStorage.setItem("savedInput3PM", blankInputArray[6].value);
-    console.log("btn 7 worked :  " + blankInputArray[6].value)
-})
-btn8.addEventListener("click", function getSaved(){
-    localStorage.setItem("savedInput4PM", blankInputArray[7].value);
-    console.log("btn 8 worked :  " + blankInputArray[7].value)
-})
-btn9.addEventListener("click", function getSaved(){
-    localStorage.setItem("savedInput5PM", blankInputArray[8].value);
-    console.log("btn 9 worked :  " + blankInputArray[8].value)
-})
-//key is working but value comes back as " ", but console log comes back with the string i typed in... if i get rid of the stringify, it works... why?? bc already a string?? but i thought we were supposed to stringify everything. and if not needed, then is parse not needed either??
-//also, i think i can delete var blankInput1-9
-
-function getSavedInputs() {
-    //console.log("saved9am " + savedInput9AM)
-    var savedInput10AM = localStorage.getItem("savedInput9AM");
-
-}
-
-document.querySelector("#hour0").value = JSON.parse(localStorage.getItem("savedInput9AM")) || "" ;
+btn2.addEventListener("click", function getSaved() {
+  localStorage.setItem("savedInput10AM", JSON.stringify(blankInputArray[1].value));
+  console.log("btn 2 worked :  " + blankInputArray[1].value);
+});
+btn3.addEventListener("click", function getSaved() {
+  localStorage.setItem("savedInput11AM", JSON.stringify(blankInputArray[2].value));
+  console.log("btn 3 worked :  " + blankInputArray[2].value);
+});
+btn4.addEventListener("click", function getSaved() {
+  localStorage.setItem("savedInput12PM", JSON.stringify(blankInputArray[3].value));
+  console.log("btn 4 worked :  " + blankInputArray[3].value);
+});
+btn5.addEventListener("click", function getSaved() {
+  localStorage.setItem("savedInput1PM", JSON.stringify(blankInputArray[4].value));
+  console.log("btn 5 worked :  " + blankInputArray[4].value);
+});
+btn6.addEventListener("click", function getSaved() {
+  localStorage.setItem("savedInput2PM", JSON.stringify(blankInputArray[5].value));
+  console.log("btn 6 worked :  " + blankInputArray[5].value);
+});
+btn7.addEventListener("click", function getSaved() {
+  localStorage.setItem("savedInput3PM", JSON.stringify(blankInputArray[6].value));
+  console.log("btn 7 worked :  " + blankInputArray[6].value);
+});
+btn8.addEventListener("click", function getSaved() {
+  localStorage.setItem("savedInput4PM", JSON.stringify(blankInputArray[7].value));
+  console.log("btn 8 worked :  " + blankInputArray[7].value);
+});
+btn9.addEventListener("click", function getSaved() {
+  localStorage.setItem("savedInput5PM", JSON.stringify(blankInputArray[8].value));
+  console.log("btn 9 worked :  " + blankInputArray[8].value);
+});
 
 
+//get and print saved data
+document.querySelector("#hour0").value = JSON.parse(localStorage.getItem("savedInput9AM")) || "";
+document.querySelector("#hour1").value = JSON.parse(localStorage.getItem("savedInput10AM")) || ""; 
+document.querySelector("#hour2").value = JSON.parse(localStorage.getItem("savedInput11AM")) || "";
+document.querySelector("#hour3").value = JSON.parse(localStorage.getItem("savedInput12PM")) || "";
+document.querySelector("#hour4").value = JSON.parse(localStorage.getItem("savedInput1PM")) || "";
+document.querySelector("#hour5").value = JSON.parse(localStorage.getItem("savedInput2PM")) || "";
+document.querySelector("#hour6").value = JSON.parse(localStorage.getItem("savedInput3PM")) || "";
+document.querySelector("#hour7").value = JSON.parse(localStorage.getItem("savedInput4PM")) || "";
+document.querySelector("#hour8").value = JSON.parse(localStorage.getItem("savedInput5PM")) || "";
+
+ 
 //if works, try removing btn link to method in html???
 
-/* to do:
-have save button add activities to local storage and call back on page refresh
-*/
+
